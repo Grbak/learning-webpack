2c66af2be1efe7ee6363df060a936b7105fe321e
Grbak <eden91hazard@gmail.com>
2021-10-25 01:38:26 +0300

    tried to figure out why webpack doesnt require loader for ttf

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png)$/,
+                test: /\.png$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
